c19cba7fca48c13703c40662c626287b00a3ded5
Micha Buchholz <mail@lean-stack.de>
2022-12-20 20:10:36 +0100

    Import typescript template. Initial commit.

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <header className="flex items-center px-8 sm:px-4 bg-white shadow">
+      <h1 className="py-4 text-2xl font-semibold">Lean React</h1>
+    </header>
+  );
+}
+
+export default App;
